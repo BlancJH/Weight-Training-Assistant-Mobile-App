8f5a05a6fcea266807aa0659bcde561c14a5bd92
BlancJH <abcdst@naver.com>
2024-12-04 13:01:44 +0800

    Install Java springs framework

---
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/WeightAssistantMobileAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blancJH.weight_assistant_mobile_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightAssistantMobileAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightAssistantMobileAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blancJH/weight_assistant_mobile_app_backend/WeightAssistantMobileAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blancJH.weight_assistant_mobile_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightAssistantMobileAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
