afc7ae9c8e0bc0a0dad8a4ed2c23b16cd34ec2d1
BlancJH <abcdst@naver.com>
2024-12-09 22:26:08 +0800

    Create Register screen structure

---
+++ frontend/lib/screens/register_screen.dart
@@ -0,0 +1,14 @@
+// Sample register screen
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Center(
+        child: Text('This is the registration screen.'),
+      ),
+    );
+  }
+}
