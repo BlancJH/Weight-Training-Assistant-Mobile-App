068950e3b2e424983c0d5e6ab32edb375704b80b
BlancJH <abcdst@naver.com>
2024-12-10 14:36:03 +0800

    Add Name textfields

--- frontend/lib/screens/register_screen.dart
+++ frontend/lib/screens/register_screen.dart
@@ -12,13 +12,34 @@ class RegisterScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
+            TextField(decoration: InputDecoration( //First Name field
+              labelText: 'First Name',
+               border: OutlineInputBorder(),
+              hintText: 'Enter your First Name',
+            ),
+            keyboardType: TextInputType.name,
+            textCapitalization: TextCapitalization.words,
+            autocorrect: false, // Disables autocorrect or suggestions
+            ),
+            SizedBox(height: 16.0),
+            TextField( // Last Name field
               decoration: InputDecoration(
+                labelText: 'Last Name',
+                border: OutlineInputBorder(),
+                hintText: 'Enter your Last Name',
+              ),
+              keyboardType: TextInputType.emailAddress, // Optimised keyboard for email
+              autocorrect: false,
+            ),
+            SizedBox(height: 16.0),
+            TextField( // Email field
+              decoration: InputDecoration( 
                 labelText: 'Email',
                 border: OutlineInputBorder(),
                 hintText: 'Enter your email',
               ),
-              keyboardType: TextInputType.emailAddress, // Optimized for email
+              keyboardType: TextInputType.emailAddress, // Optimised keyboard for email
+              autocorrect: false,
             ),
           ],
         ),
