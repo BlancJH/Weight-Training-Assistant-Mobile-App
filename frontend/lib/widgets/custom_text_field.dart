133daef5dcb9a3e4b1c19733383ec4d8f6f1a448
BlancJH <abcdst@naver.com>
2024-12-12 16:10:29 +0800

    Create custom text field widget

---
+++ frontend/lib/widgets/custom_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  const CustomTextField({
+    required this.labelText,
+    required this.controller,
+    this.validator,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(),
+      ),
+      obscureText: obscureText,
+      validator: validator,
+    );
+  }
+}
