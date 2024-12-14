b9d2c05540ce7c6f9dfe940a27193aef52edd0bd
BlancJH <abcdst@naver.com>
2024-12-14 13:00:34 +0800

    Create: MemoryUserRepository

---
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/repository/MemoryUserRepository.java
@@ -0,0 +1,38 @@
+import java.util.Optional;
+
+public class MemoryUserRepository implements UserRepository {
+
+    private static Map<String, User> store = new Hashmap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public User save(User user) {
+        user.setId(++sequence);
+        store.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return store.value().stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findAny();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return store.value().stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new Array<>(store.value());
+    }
+}   
