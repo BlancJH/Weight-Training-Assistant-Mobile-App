846fb2b4f2d3d75710810a205c0a56b875a0789a
BlancJH <abcdst@naver.com>
2024-12-14 12:40:16 +0800

    Refactor: UserRepository

--- backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/repository/UserRepository.java
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.blancJH.weight_assistant_mobile_app_backend.repository;
 
 import com.blancJH.weight_assistant_mobile_app_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User user);
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    List<User> findAll();
 }
