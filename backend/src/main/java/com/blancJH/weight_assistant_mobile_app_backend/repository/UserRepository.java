64c7a8effd7dadbec74428444d90928fd8f19af5
BlancJH <abcdst@naver.com>
2024-12-15 13:59:12 +0800

    Bugfix: Delete JPA repository

--- backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/repository/UserRepository.java
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.blancJH.weight_assistant_mobile_app_backend.repository;
 
 import com.blancJH.weight_assistant_mobile_app_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository {
     User save(User user);
     Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
