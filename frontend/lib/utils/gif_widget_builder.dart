79b3a2f99a0a240afe36535a9eaf6754ae8d6254
BlancJH <abcdst@naver.com>
2024-12-21 13:07:35 +0800

    .

---
+++ frontend/lib/utils/gif_widget_builder.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../widgets/gif_widget.dart';
+import '../models/exercise_gif.dart';
+
+class GifWidgetBuilder extends StatelessWidget {
+  final List<ExerciseGifModel> gifData;
+
+  const GifWidgetBuilder({
+    required this.gifData,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: gifData
+          .map((gif) => Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: GifWidget(
+                  gifUrl: gif.gifUrl,
+                  text: gif.text,
+                  optionalText: gif.optionalText,
+                  width: 300, // Set custom width
+                  height: 150, // Set custom height
+                ),
+              ))
+          .toList(),
+    );
+  }
+}
