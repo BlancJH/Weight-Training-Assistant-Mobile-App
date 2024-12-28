13ea36b9fa60867d8ed3d9c6d07e5010152edc5a
BlancJH <abcdst@naver.com>
2024-12-28 18:36:49 +0800

    Create Exercise model

---
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/Exercise.java
@@ -0,0 +1,31 @@
+package com.blancJH.weight_assistant_mobile_app_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "exercises")
+@Getter @Setter
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long exerciseId; // System-generated ID
+
+    @Column(nullable = false)
+    private String exerciseName; // Name of the exercise
+
+    @Column(nullable = false)
+    private String exerciseCategory; // Category (e.g., "Machine", "Cardio", "Bodyweight", "Barbell", "Dumbbell", "Assisted Bodyweight")
+
+    @ElementCollection // Allows storing a list of strings
+    @CollectionTable(name = "exercise_muscles", joinColumns = @JoinColumn(name = "exercise_id"))
+    @Column(name = "muscle")
+    private List<String> muscles; // Targeted muscles (e.g., ["Chest", "Triceps"])
+
+    @Column(nullable = false)
+    private String exerciseGifUrl; // URL for the exercise's GIF
+}
