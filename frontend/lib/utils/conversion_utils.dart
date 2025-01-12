759fba51131956c3e1effed0763203b2309072a4
BlancJH <abcdst@naver.com>
2025-01-12 09:26:47 +0800

    Add weight unit conversions

--- frontend/lib/utils/conversion_utils.dart
+++ frontend/lib/utils/conversion_utils.dart
@@ -8,4 +8,14 @@ class ConversionUtils {
   static double feetToCm(double feet) {
     return (feet * 30.48).ceilToDouble(); // Conversion factor with rounding up
   }
+
+  // Convert weight from kg to lbs
+  static double kgToLbs(double kg) {
+    return kg * 2.20462; // Conversion factor
+  }
+
+  // Convert weight from lbs to kg
+  static double lbsToKg(double lbs) {
+    return lbs / 2.20462; // Conversion factor
+  }
 }
