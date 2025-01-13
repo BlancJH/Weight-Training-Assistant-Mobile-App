89cf617249cc5a8362b4a404825e65c7b9514857
BlancJH <abcdst@naver.com>
2025-01-13 13:25:58 +0800

    Add change tracking

--- frontend/lib/widgets/custom_text_field.dart
+++ frontend/lib/widgets/custom_text_field.dart
@@ -5,12 +5,14 @@ class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String? Function(String?)? validator;
   final bool obscureText;
+  final void Function(String)? onChanged; // Added onChanged callback
 
   const CustomTextField({
     required this.labelText,
     required this.controller,
     this.validator,
     this.obscureText = false,
+    this.onChanged, // Optional parameter for onChanged
   });
 
   @override
@@ -25,6 +27,7 @@ class CustomTextField extends StatelessWidget {
       validator: validator,
       autocorrect: false,
       enableSuggestions: false,
+      onChanged: onChanged, // Trigger callback when text changes
     );
   }
 }
