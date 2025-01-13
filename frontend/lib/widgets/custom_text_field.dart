d7deb0fefc730827781b2a2abe6b3593a6b20711
BlancJH <abcdst@naver.com>
2025-01-13 15:12:45 +0800

    Add max charactor counter

--- frontend/lib/widgets/custom_text_field.dart
+++ frontend/lib/widgets/custom_text_field.dart
@@ -5,29 +5,46 @@ class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String? Function(String?)? validator;
   final bool obscureText;
-  final void Function(String)? onChanged; // Added onChanged callback
+  final void Function(String)? onChanged;
+  final int? maxLength; // Optional max length for character count
 
   const CustomTextField({
     required this.labelText,
     required this.controller,
     this.validator,
     this.obscureText = false,
-    this.onChanged, // Optional parameter for onChanged
+    this.onChanged,
+    this.maxLength, // Optional parameter for max length
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: labelText,
-        border: OutlineInputBorder(),
-      ),
-      obscureText: obscureText,
-      validator: validator,
-      autocorrect: false,
-      enableSuggestions: false,
-      onChanged: onChanged, // Trigger callback when text changes
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: labelText,
+            border: const OutlineInputBorder(),
+            counterText: '', // Hide default counter
+          ),
+          obscureText: obscureText,
+          validator: validator,
+          onChanged: onChanged,
+        ),
+        if (maxLength != null)
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              '${controller.text.length} / $maxLength',
+              style: const TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
