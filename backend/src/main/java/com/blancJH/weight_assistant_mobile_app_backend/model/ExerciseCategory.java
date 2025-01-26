ac961fafdeeaa61934e5b092e16c9eadeb61aab6
BlancJH <abcdst@naver.com>
2025-01-27 00:34:53 +0800

    Update Exercise category enum

---
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/ExerciseCategory.java
@@ -0,0 +1,14 @@
+package com.blancJH.weight_assistant_mobile_app_backend.model;
+
+public enum ExerciseCategory {
+    
+    MACHINE,
+    CARDIO,
+    BODYWEIGHT,
+    BARBELL,
+    DUMBBELL,
+    ASSISTED_BODYWEIGHT
+}
+
+
+
