2e44c7d836d5eba5a5c3c8f61454fb26f63a7407
BlancJH <abcdst@naver.com>
2025-01-29 09:44:51 +0800

    Refactor db design

--- backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/WorkoutPlan.java
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/WorkoutPlan.java
@@ -1,7 +1,9 @@
 package com.blancJH.weight_assistant_mobile_app_backend.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,16 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "workout_plans")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 public class WorkoutPlan {
 
     @Id
@@ -32,20 +33,13 @@ public class WorkoutPlan {
     @Column(name = "planned_date", nullable = false)
     private LocalDate plannedDate;
 
-    @Column(name = "split", nullable = false)
-    private String split;
-
-    @Column(name = "exercises", columnDefinition = "TEXT", nullable = false)
-    private String exercises;
-
     @Column(name = "status", nullable = false)
     private boolean status; // true = done, false = not done
 
-    public WorkoutPlan(User user, LocalDate plannedDate, String split, String exercises, boolean status) {
-        this.user = user;
-        this.plannedDate = plannedDate;
-        this.split = split;
-        this.exercises = exercises;
-        this.status = status;
-    }
+    @Column(nullable = false)
+    private String splitName;
+
+    @OneToMany(mappedBy = "workoutPlan", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkoutPlanExercise> exercises;
+
 }
