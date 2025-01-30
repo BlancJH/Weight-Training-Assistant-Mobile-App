36d7df91646f370b2442d0ab35d4bfc458effa65
BlancJH <abcdst@naver.com>
2025-01-30 09:36:27 +0800

    Migrate workout plan exercise to postgreSQL

--- backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/WorkoutPlanExercise.java
+++ backend/src/main/java/com/blancJH/weight_assistant_mobile_app_backend/model/WorkoutPlanExercise.java
@@ -1,13 +1,14 @@
 package com.blancJH.weight_assistant_mobile_app_backend.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,11 +27,11 @@ public class WorkoutPlanExercise {
     @Column(nullable = false)
     private Integer reps;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "exercise_id", nullable = false)
     private Exercise exercise;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "workout_plan_id", nullable = false)
     private WorkoutPlan workoutPlan;
 
